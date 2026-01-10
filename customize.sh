4819ad74f8e768bb329e53c5d7a8609f1fe4dde1
glmghost <62704244+glmghost@users.noreply.github.com>
2026-01-10 12:14:50 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #===============================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.238/g' openwrt/package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/kenzo/g' openwrt/package/base-files/files/bin/config_generate
+
+# 仅启用 SSR Plus 集成 NaiveProxy
+echo "CONFIG_PACKAGE_naiveproxy=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NaiveProxy=y" >> .config
 
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
