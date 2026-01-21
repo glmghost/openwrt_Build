ac8c3d681cb117d10e09da385adcae936d3b848a
glmghost <62704244+glmghost@users.noreply.github.com>
2026-01-21 18:44:58 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 #===============================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.31.238/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.31.238/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.31.238/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
